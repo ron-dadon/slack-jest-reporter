30e933e2c645103fc91fc873621fabb984aa0378
Ron Dadon <rondadon1@gmail.com>
2022-11-24 10:09:04 +0200

    Version 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.2.0] - 2022-11-24
+
+### Added
+
+- Progress message - report progress by updating the main thread message
+- Support for attaching screenshots if such exists automatically to the test thread
+
+## [2.1.0] - 2022-11-23
+
+First package release! 🥳
+
+- Create a thread when the tests run starts
+- Post test case results as replies to the thread
+- Update thread with results when done
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const fs = require('fs')
 const { WebClient } = require('@slack/web-api')
 
 const TEXTS = {
   runStart: 'Tests started',
   runCompletePassed: 'Tests passed',
   runCompleteFailed: 'Tests failed',
+  runProgress: 'Tests running {progress}%',
   testCaseResultPassed: '{testName} - passed in {duration}ms',
   testCaseResultFailed: '{testName} - failed in {duration}ms'
 }
@@ -13,7 +15,11 @@ const defaultOptions = {
   slackChannelId: null,
   slackBotTokenEnvVar: 'SLACK_BOT_TOKEN',
   slackChannelIdEnvVar: 'SLACK_CHANNEL_ID',
-  texts: {}
+  texts: {},
+  attachScreenshots: false,
+  attachScreenshotsOnlyOnFail: true,
+  screenshotsPattern: '^{testName}\\.jpg$',
+  screenshotsPath: './',
 }
 
 class SlackReporter {
@@ -52,6 +58,37 @@ class SlackReporter {
       ...options,
     }
     return this._slack.chat.postMessage(payload)
+  }
+
+  async _postScreenshots({ testName }) {
+    if (!this._slack) return
+    if (!this._options.attachScreenshots) return
+
+    const files = fs.readdirSync(this._options.screenshotsPath)
+    if (!files.length) return
+
+    const pattern = this._options.screenshotsPattern.replaceAll('{testName}', testName)
+    const matcherRegex = new RegExp(pattern)
+    const matchingFiles = files.filter((file) => matcherRegex.test(file))
+    if (!matchingFiles.length) return
+
+    const results = await Promise.allSettled(matchingFiles.map((file) => {
+      const data = fs.readFileSync(`${this._options.screenshotsPath}${file}`)
+      return this._slack.files.uploadV2({
+        channel: this._slackChannelId,
+        thread_ts: this._slackThreadTs,
+        file: data,
+        filename: file,
+      })
+    }))
+
+    results.forEach(({ status, reason }, i) =>
+      status === 'rejected'
+      && console.warn(
+        `slack-jest-reporter: failed to upload screenshot ${matchingFiles[i]}: ${reason.message}`,
+        reason
+        )
+    )
   }
 
   async _updateMessage(options) {
@@ -99,8 +136,25 @@ class SlackReporter {
         .replaceAll('{testName}', testCaseResult.fullName)
         .replaceAll('{duration}', testCaseResult.duration)
       await this._postReply({ text })
+      if (this._options.attachScreenshotsOnlyOnFail && testCaseResult.status === 'passed') return
+      await this._postScreenshots({ testName: testCaseResult.fullName })
     } catch (e) {
       console.warn('slack-jest-reporter: failed to publish run test case result', e)
+    }
+  }
+
+  async onTestResult(_, __, result) {
+    const { numPassedTestSuites: passed, numFailedTestSuites: failed, numTotalTestSuites: total } = result
+    const progress = Math.round((passed + failed) / total * 100)
+    try {
+      await this._updateMessage({
+        text: typeof this._texts.runProgress === 'function'
+          ? this._texts.runProgress({ passed, failed, total, progress })
+          : this._texts.runProgress.replaceAll('{progress}', progress),
+        link_names: 1,
+      })
+    } catch (e) {
+      console.warn('slack-jest-reporter: failed to publish run progress message', e)
     }
   }
 }
